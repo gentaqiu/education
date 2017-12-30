5d0943e9ac10496dd78c1ca1dfe59568f0b1922a
Ken Qiu <kqiu@drmg.ca>
2017-12-30 17:31:01 -0500

    fix

--- server/models/script.sh
+++ server/models/script.sh
@@ -1,1 +1,6 @@
-/usr/bin/node /opt/bitnami/apps/education/server.js&
+/usr/bin/node /opt/bitnami/apps/education/start.js
+
+db.subject.update({"_id" :ObjectId("5a4775e340344c03c678a739") },{$set : {"lang":"zh"}})
+
+
+db.subject.dropIndex({"v" : 2,"unique" : true,"key" : {"lang" : 1},"name" : "lang_1","ns" : "education.subject","background" : true})
